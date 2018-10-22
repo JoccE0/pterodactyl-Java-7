b3dc1ca48746f7284512f5fb9f2d0eb05e2ea48d
Pajkanon <Jocce@jocce.nu>
2018-10-22 12:42:34 +0200

    Create entrypoint.sh

---
+++ entrypoint.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+sleep 5
+
+cd /home/container
+
+# Output Current Java Version
+java -version
+
+# Replace Startup Variables
+MODIFIED_STARTUP=`eval echo $(echo ${STARTUP} | sed -e 's/{{/${/g' -e 's/}}/}/g')`
+echo ":/home/container$ ${MODIFIED_STARTUP}"
+
+# Run the Server
+${MODIFIED_STARTUP}
